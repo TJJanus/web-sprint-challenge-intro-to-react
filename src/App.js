997ac7198dc1d0e41ff8f4efe3909533e0a331bd
TJ Janus <TJJanus1@gmail.com>
2020-09-11 12:39:47 -0400

    working

--- src/App.js
+++ src/App.js
@@ -1,17 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios'
+import Character from './components/Character'
+import styled from 'styled-components';
+
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  const [cards, setCards] = useState([])
+
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  useEffect(() => {
+    axios.get('https://rickandmortyapi.com/api/character/')
+      .then (res =>{
+        console.log(res.data.results);
+        setCards(res.data.results);
+      })
+  },[])
+
+  
+
+  
+  
+
+
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+        <img src='https://help.redbubble.com/hc/article_attachments/360002309526/Rick_and_Morty_-_logo__English_.png' alt='Ricky and Morty Character'/>
+        {/* <Header1 className="Header">Characters</Header1> */}
+          <div>
+          {cards.map(item => {
+            return (
+
+              <div>
+                  <Character name={item.name} species={item.species} gender={item.gender} image={item.image} status={item.status}/>
+                  
+              </div>
+            )
+          })}
+
+
+          </div>
+    
     </div>
   );
 }
